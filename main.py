bfa1b0576b19eea5254753cf07b935f3a97fb0d3
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-10-25 07:40:29 +0100

    refactor: streamline file reading and processing logic

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
+from collections import defaultdict
 import json
 
-def read_file(filename):
-    try:
-        file = open(filename, 'r')
-        lines = [line for line in file if not line == '\n']
-    except FileNotFoundError as err:
-        print(f'Something went wrong: {err}')
-    finally:
-        file.close()
+lines: list[str] = []
 
-    return lines
+try:
+    with open('readme.md', 'r') as file:
+        for line in file:
+            if not line == '\n': lines.append(line)
+except FileNotFoundError as err:
+    print(f'Something went wrong: {err}')
 
+items = defaultdict(list)
+heading = 'No heading'
 
-if __name__ == '__main__':
-    lines = read_file('readme.md')
-    items = {}
-    
-    for line in lines:
-        if line.startswith('##'):
-            heading = line[3:-1]
-            list = []
+for line in lines:
+    if line.startswith('##'):
+        heading = line[:-1]
 
-        if line.startswith('-'):
-            list.append(line[2:-1])
-            items[heading] = list
+    if line.startswith('-'):
+        items[heading].append(line[:-1])
 
-    json_string = json.dumps(items, indent=4)
-    print(json_string)
+json_string = json.dumps(items, indent=4)
+print(json_string)
