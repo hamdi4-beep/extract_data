fccf91f8a62abd89a363323b2f24bdbe23670074
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-10-25 08:13:46 +0100

    refactor: simplify line processing and heading management

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 from collections import defaultdict
 import json
 
-lines: list[str] = []
+items = defaultdict(list)
+heading = 'No heading'
 
 try:
     with open('readme.md', 'r') as file:
         for line in file:
-            if not line == '\n': lines.append(line)
+            if line.startswith('#'):
+                heading = line.strip()
+
+            if line.startswith('-'):
+                line = line.strip()
+                items[heading].append(line)
 except FileNotFoundError as err:
     print(f'Something went wrong: {err}')
-
-items = defaultdict(list)
-heading = 'No heading'
-
-for line in lines:
-    if line.startswith('##'):
-        heading = line[3:-1].strip()
-
-    if line.startswith('-'):
-        line = line[1:-1].strip()
-        items[heading].append(line)
 
 json_string = json.dumps(items, indent=4)
 print(json_string)
