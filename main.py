bc03e1a98665c136c3851460accacca33ec2c9aa
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-10-25 08:31:59 +0100

    fix: correct heading and line processing to handle leading characters

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ try:
     with open('readme.md', 'r') as file:
         for line in file:
             if line.startswith('#'):
-                heading = line.strip()
+                # skips the first three hashtag characters
+                heading = line[3:].strip()
 
             if line.startswith('-'):
-                line = line.strip()
+                line = line[1:].strip()
+                items[heading].append(line)
+
+            if line[0].isdigit():
+                # skips the digit number and period which accounts for numbered lists
+                line = line[2:].strip()
                 items[heading].append(line)
 except FileNotFoundError as err:
     print(f'Something went wrong: {err}')
