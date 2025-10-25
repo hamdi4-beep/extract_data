fd7acdc6ab1ab99e2365cff26c6f8e9d1548a322
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-10-25 08:40:42 +0100

    fix: improve line processing to handle list items and ensure proper heading assignment

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ try:
                 # skips the first three hashtag characters
                 heading = line[3:].strip()
 
-            if line.startswith('-'):
-                line = line[1:].strip()
-                items[heading].append(line)
-
-            if line[0].isdigit():
-                # skips the digit number and period which accounts for numbered lists
-                line = line[2:].strip()
+            if not line.startswith('#') and line != '\n':
+                line = line.strip()
                 items[heading].append(line)
 except FileNotFoundError as err:
     print(f'Something went wrong: {err}')
 
 json_string = json.dumps(items, indent=4)
-print(json_string)
+filename = 'output.json'
+
+with open(filename, 'w') as output_file:
+    output_file.write(json_string)
+
+print(f'{filename} saved!')
